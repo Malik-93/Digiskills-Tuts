a08ffcce2d445d34b14c5f825b45786ca87bdb73
M Mudassir <Malikjrw147@yahoo.com>
2019-04-26 10:09:49 -0700

    Redux BoilerPlate folder Added inside src folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Todos from './components/Todos';
 
 function App() {
   return (
+      <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Hello React</h1>
+    <Link to='/todos' >Todos</Link>
+      <Switch>
+      <Route path = '/todos' component={ Todos } />
+      </Switch>
     </div>
+    </Router>
   );
 }
 
